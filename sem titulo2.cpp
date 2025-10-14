70f64ff41f5d853838d3b56f8ca5f98adb6f9141
Pauloss santos <snttsspaulo@gmail.com>
2025-10-14 08:20:48 -0300

    Add files via upload

---
+++ sem titulo2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+int main() {
+    float numero; 
+
+    
+    printf("Digite um numero real: ");    
+    scanf("%f", &numero);  
+    printf("O numero digitado foi: %f\n", numero);
+
+    return 0; 
+}
+
